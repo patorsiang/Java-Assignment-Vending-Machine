662811b5124a9ad58fd40027c39f416be268a3f2
Napatchol Thaipanich <napatchol.tha@gmail.com>
2024-10-09 21:40:36 +0100

    feat(app): fix Item test

--- VendingMachine/src/ItemTest.java
+++ VendingMachine/src/ItemTest.java
@@ -2,67 +2,75 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ItemTest {
+
+    static Item item;
+
+    @BeforeAll
+    static void setUp() {
+        item = new Item("01", "Coke", 1.25);
+    }
 
     @Test
     void testGetCode() {
-        Item item = new Item("01", "Coke", 1.25);
         assertEquals("01", item.getCode());
     }
 
     @Test
+    @Order(1)
+    void testPrintItem() {
+        assertEquals("01 Coke ￡1.25 0", item.printItem());
+    }
+
+    @Test
+    @Order(2)
     void testChangePriceSuccess() {
-        Item item = new Item("01", "Coke", 1.25);
         item.changePrice(1.5);
         assertEquals("01 Coke ￡1.50 0", item.printItem());
     }
 
     @Test
     void testChangePriceFail() {
-        Item item = new Item("01", "Coke", 1.25);
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> item.changePrice(-1.5));
         assertEquals("Price cannot be negative", exception.getMessage());
     }
 
     @Test
+    @Order(3)
     void testIncreaseQuantitySuccess() throws OverflowingShelfException {
-        Item item = new Item("01", "Coke", 1.25);
         item.increaseQuantity(10);
-        assertEquals("01 Coke ￡1.25 10", item.printItem());
+        assertEquals("01 Coke ￡1.50 10", item.printItem());
     }
 
     @Test
     void testIncreaseQuantityFail() {
-        Item item = new Item("01", "Coke", 1.25);
         Throwable exception = assertThrows(OverflowingShelfException.class, () -> item.increaseQuantity(100));
         assertEquals("This item, 01, reached the limit of its' shelf.", exception.getMessage());
     }
 
     @Test
     void testIncreaseQuantityFail2() {
-        Item item = new Item("01", "Coke", 1.25);
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> item.increaseQuantity(-2));
         assertEquals("Quantity cannot be negative", exception.getMessage());
     }
 
     @Test
+    @Order(4)
     void testDecreaseQuantitySuccess() throws NotEnoughException, OverflowingShelfException {
-        Item item = new Item("01", "Coke", 1.25);
-        item.increaseQuantity(3);
         item.decreaseQuantity(2);
-        assertEquals("01 Coke ￡1.25 1", item.printItem());
+        assertEquals("01 Coke ￡1.50 8", item.printItem());
     }
 
     @Test
+    @Order(5)
     void testDecreaseQuantityFail() {
-        Item item = new Item("01", "Coke", 1.25);
         Throwable exception = assertThrows(NotEnoughException.class, () -> item.decreaseQuantity(10));
         assertEquals("This item, 01, have not enough stock as you requested.", exception.getMessage());
     }
 
     @Test
     void testDecreaseQuantityFail2() {
-        Item item = new Item("01", "Coke", 1.25);
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> item.decreaseQuantity(-1));
         assertEquals("Quantity cannot be negative", exception.getMessage());
     }
